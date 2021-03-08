4ead8cc2efcd8d0b20d2d3aae83bebab3e0a66a7
Lavanya1019 <lavanya.ms@tecnotree.com>
2021-03-08 06:23:39 +0000

    TestNG Basics

---
+++ src/test2/ParameterEx.java
@@ -0,0 +1,19 @@
+package test2;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.Parameters;
+
+
+public class ParameterEx {
+	
+	
+	@Parameters({"nameparam"})
+	@Test
+	public void testcase(String name)
+	{
+	System.out.println("hi "+name);
+	}
+	}
+
+
---
+++ src/test2/TestCase2.java
@@ -0,0 +1,15 @@
+package test2;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	
+	
+	@Test
+	public void method2() {
+		System.out.println("Test case is execuited");
+	
+}
+
+}
+
